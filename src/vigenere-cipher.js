c19999b30b4e17d33795e31290a068fd59b031ff
kostik4kg <kostik4kg@gmail.com>
2020-10-30 18:10:12 +0300

    feat: add vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,14 +1,65 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(aaa=true){
+    this.aaa = aaa;
   }
+  encrypt(str, key) {
+    if(str === undefined || key === undefined){
+      throw new Error();
+    }else{
+    let alf = 'abcdefghijklmnopqrstuvwxyz';
+    let cipher = '';
+    let str2 = str.toLowerCase();
+    let key2 = key.toLowerCase();
+    let indKey = 0;
+    while (str.length > key2.length) {
+      key2 += key2;
+    }
+    for(let i = 0; i< str.length; i++){
+      if(str2[i].charCodeAt() < 97 || str2[i].charCodeAt() > 122){
+        cipher += str2[i];
+        indKey--;
+      }else{
+        cipher += alf[((key2[indKey + i].charCodeAt()-97) + (str2[i].charCodeAt()-97)) % 26];
+    }
+    }
+    if (!this.aaa) {
+      return cipher.toUpperCase().split('').reverse().join('')
+    }else{
+    return cipher.toUpperCase();
+  }}
+  }    
+  decrypt(str, key) {
+
+    if (str === undefined || key === undefined) {
+      throw new Error();
+    }
+    else{
+    let alf = 'abcdefghijklmnopqrstuvwxyz';
+    let cipher = '';
+    let str2 = str.toLowerCase();
+    let key2 = key.toLowerCase();
+    let indKey = 0;
+    while (str.length > key2.length) {
+      key2 += key2;
+    }
+    for (let i = 0; i < str2.length; i++) {
+
+      if (str2[i].charCodeAt() < 97 || str2[i].charCodeAt() > 122) {
+        cipher += str2[i];
+        indKey--;
+      } else {
+        cipher += alf[((str2[i].charCodeAt() - 97) + 26 - (key2[indKey + i].charCodeAt() - 97)) % 26];
+      }
+    }
+    if(!this.aaa){
+      return cipher.toUpperCase().split('').reverse().join('')
+    }else{
+    return cipher.toUpperCase();
+    }
+  }
+}
 }
 
 module.exports = VigenereCipheringMachine;
